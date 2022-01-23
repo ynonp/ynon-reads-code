7b9c6af508b25ed7619c1209caeaa212a60becfe
ynonp <ynonperek@gmail.com>
2022-01-23 22:19:59 +0200

    initial commit

---
+++ data/siteMetadata.js
@@ -0,0 +1,34 @@
+const siteMetadata = {
+  title: 'Ynon Reads Code',
+  author: 'Ynon Perek',
+  headerTitle: 'ynon-reads-code',
+  description: 'Reading and Learning Open Source Libraries',
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteLogo: '/static/images/logo.png',
+  image: '/static/images/avatar.png',
+  email: 'ynon@ynonperek.com',
+  github: 'https://github.com/ynonp',
+  locale: 'en-US',
+  analytics: {
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+  },
+  newsletter: {
+    // supports mailchimp, buttondown, convertkit, klaviyo
+    // Please add your .env file and modify it according to your selection
+    provider: '',
+  },
+  comment: {
+    // Select a provider and use the environment variables associated to it
+    // https://vercel.com/docs/environment-variables
+    provider: 'disqus', // supported providers: giscus, utterances, disqus
+    disqusConfig: {
+      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+      shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+    },
+  },
+}
+
+module.exports = siteMetadata
